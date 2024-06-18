01f026e7590f6d25e69401923cc3d5d4fa79e8ed
billie <116457257+billiestr@users.noreply.github.com>
2024-06-18 09:54:26 +0000

    began working on simulation design

--- simulations/projectiles/script.js
+++ simulations/projectiles/script.js
@@ -13,11 +13,30 @@ slider.oninput = function() {}*/
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 // Draw a rectangle to make the canvas visible
-ctx.fillStyle = 'lightgray';
-ctx.fillRect(10, 10, canvas.width - 20, canvas.height - 20);
+ctx.fillStyle = 'blue';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 import * as drawShape from '/utils/shape.js'
 
-drawShape.circle(ctx, {x: canvas.width /2, y: canvas.height /2}, canvas.height /2.3)
+//drawShape.circle(ctx, {x: canvas.width /2, y: canvas.height /2}, canvas.height /2.3)
+
+let i = 0;
+let lastTime = Date.now()
+function loop() {
+	const now = Date.now()
+	const deltaTime = (now-lastTime)/1000
+	lastTime = now
+	if (Math.random() > 0.95) {console.log(1/deltaTime)}
+	ctx.fillStyle = 'blue';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	drawShape.sineWave(ctx, [10, 100], {offset:i})
+	drawShape.sineWave(ctx, [10, 100], {offset:-i})
+	drawShape.drawFunction(ctx, (x)=>{
+		return Math.sin((x+i)*Math.PI/180)+Math.sin((x-i)*Math.PI/180)
+	}, {}, {offset:[10, 100], scale: [1, 50]})
+	i++;
+	requestAnimationFrame(loop)
+}
+loop()
 
 console.log("yeah")
