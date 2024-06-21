96954cfda32e3b7071c4f4f0fb2ccc900d15a86a
billie <116457257+billiestr@users.noreply.github.com>
2024-06-21 10:57:07 +0000

    moved wave stuff to right place

--- simulations/projectiles/script.js
+++ simulations/projectiles/script.js
@@ -7,36 +7,14 @@ const startButton = document.getElementById("flappy-start")
 const playButton = document.getElementById("flappy-play")
 const scoreText = document.getElementById("flappy-score")
 const highScoreText = document.getElementById("flappy-hscore")
-
-const slider = document.getElementById("lifespan-slider")
-slider.oninput = function() {}*/
-canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;
+*/
+canvas.width = 600;
+canvas.height = 500;
 // Draw a rectangle to make the canvas visible
 ctx.fillStyle = 'blue';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 import * as drawShape from '/utils/shape.js'
 
-//drawShape.circle(ctx, {x: canvas.width /2, y: canvas.height /2}, canvas.height /2.3)
+drawShape.circle(ctx, {x: canvas.width /2, y: canvas.height /2}, canvas.height /2.3)
 
-let i = 0;
-let lastTime = Date.now()
-function loop() {
-	const now = Date.now()
-	const deltaTime = (now-lastTime)/1000
-	lastTime = now
-	if (Math.random() > 0.95) {console.log(1/deltaTime)}
-	ctx.fillStyle = 'blue';
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	drawShape.sineWave(ctx, [10, 100], {offset:i})
-	drawShape.sineWave(ctx, [10, 100], {offset:-i})
-	drawShape.drawFunction(ctx, (x)=>{
-		return Math.sin((x+i)*Math.PI/180)+Math.sin((x-i)*Math.PI/180)
-	}, {}, {offset:[10, 100], scale: [1, 50]})
-	i++;
-	requestAnimationFrame(loop)
-}
-loop()
-
-console.log("yeah")
