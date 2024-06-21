56ac617762d7eb64de9eb92caabaf2a2f979136b
billie <116457257+billiestr@users.noreply.github.com>
2024-06-21 10:56:07 +0000

    continued working on function graph function

--- utils/shape.js
+++ utils/shape.js
@@ -1,23 +1,31 @@
 export function sineWave(ctx, startPos, {stretchFactor=1, offset=0,colour="black"}={}) {
 	drawFunction(ctx, (x) => {
 		return Math.sin(x*Math.PI/180)
-	}, {start:offset, end:360+offset}, {colour, offset:startPos, scale: [stretchFactor, 50]})
+	}, {start:offset}, {colour, offset:startPos, scale: [stretchFactor, 50]})
 }
 
-export function drawFunction(ctx, func, {start=0, end=360, step=1}={}, {offset=[0, 0], scale=[1,1], colour="black", width=2}={}) {
-	ctx.save()
-	ctx.fillStyle = colour;
-	ctx.lineWidth = width;
+export function drawFunction(
+	ctx, func, 
+	{start=0, end=Infinity, maxlength=600, step=60}={}, 
+	{offset=[0, 0], scale=[1,1], colour="black", width=2}={}
+	) {
 	
+	end = Math.min(end, ((maxlength-offset[0])/scale[0]) + start)
+	
+	ctx.save()
+	ctx.strokeStyle = colour;
+	ctx.lineWidth = width;
 	ctx.beginPath();
 	
 	let nextPoint = [offset[0], func(start) * scale[1] + offset[1]];
 	for (let x = start; x < end; x+=step) {
 		ctx.moveTo(...nextPoint);
-		nextPoint = [x * scale[0] + offset[0]-start, func(x) * scale[1] +offset[1]];
+		nextPoint = [(x-start) * scale[0] + offset[0], func(x) * scale[1] +offset[1]];
 		ctx.lineTo(...nextPoint);
 	}
 	ctx.stroke();
+	
+	ctx.restore()
 }
 
 export function line(ctx, point1, point2, colour='red') {
