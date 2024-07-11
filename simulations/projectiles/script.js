de3aff8dcccfb7788e4d30482fbd773b203d2321
billie <116457257+billiestr@users.noreply.github.com>
2024-07-11 11:49:22 +0000

    improved cannon and added grid

--- simulations/projectiles/script.js
+++ simulations/projectiles/script.js
@@ -1,6 +1,6 @@
 import * as drawShape from '../../utils/shape.js'
-import {Vector} from '../../utils/vector.js'
-import {Asset} from '../../utils/image.js'
+import { Vector } from '../../utils/vector.js'
+import { Asset } from '../../utils/image.js'
 
 
 const canvas = document.getElementById('canvas2');
@@ -47,22 +47,43 @@ const menu = new Menu()
 const time = new Time()
 
 
-function drawGraph(offset=[20, 20]) {
+function drawGraph(offset = [20, 20]) {
 	const acceleration = -menu.gravity
-	const b = 1
 	const vel = menu.initialVelocity
-	
+
 	// s = ut + 1/2at^2, s=0
 	// t = -u +- sqrt(u^2) / 2a
-	const landTime = 2*vel.y / -acceleration;
-	const finalDistance = landTime * vel.x 
-	const maximumHeight = vel.y * landTime/2 + acceleration/2 * landTime**2/4 
+	const landTime = 2 * vel.y / -acceleration;
+	const finalDistance = landTime * vel.x
+	const maximumHeight = vel.y * landTime / 2 + acceleration / 2 * landTime ** 2 / 4
 	console.log(finalDistance, maximumHeight)
 	drawShape.drawFunction(ctx, (horizontalDistance) => {
 		const time = horizontalDistance / vel.x
-		const height = vel.y * time - menu.gravity/2 * time**2 
+		const height = vel.y * time - menu.gravity / 2 * time ** 2
 		return height;
-	}, {step: finalDistance/100, end: finalDistance}, {offset, scale:[100, -100]})
+	}, { step: finalDistance / 100, end: finalDistance }, { offset, scale: [100, -100] })
+}
+
+function getProjectileCurve() {
+	const acceleration = -menu.gravity
+	const vel = menu.initialVelocity
+	
+	const height = (time) => {
+		return vel.y * time - (menu.gravity / 2) * time ** 2
+	}
+
+	// s = ut + 1/2at^2, s=0
+	// t = -u +- sqrt(u^2) / 2a
+	const landTime = 2 * vel.y / -acceleration;
+	const finalDistance = landTime * vel.x
+	const maximumHeight = height(landTime / 2)
+	
+	return {finalDistance, maximumHeight, func: (offset, scale) => {
+		drawShape.drawParametricFunction(ctx, (time) => {
+			return new Vector(vel.x * time, height(time))
+		}, {step: landTime/100, end: landTime}, {offset, scale})
+	}}
+	
 }
 
 //drawGraph()
@@ -72,27 +93,52 @@ function loop() {
 	angle = -Math.atan2(initialVelocity.y, initialVelocity.x)
 	ctx.fillStyle = "gray";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	
+
 	time.resetDeltaTime()
 	ctx.translate(10, 300)
+
+	const {finalDistance, maximumHeight, func} = getProjectileCurve();
+	const scale = 3 / (2+Math.max(finalDistance, maximumHeight*3))
+	console.log(scale)
+	
+	const cannonEnd = new Vector(
+		170 + Math.cos(angle) * 150,
+		40 + Math.sin(angle) * 150
+	)
+
+	func(cannonEnd.scale(scale), new Vector(200*scale, -200*scale))
+	ctx.scale(scale, scale)
+
+	ctx.beginPath()
+	console.log('a',finalDistance)
+	const step = (Math.abs(finalDistance) > 5 ? 1 : 0.1)
+	for (let i = 0; i < Math.abs(finalDistance)+0.1; i += step) {
+		const startLine = cannonEnd.add2(i*200*Math.sign(finalDistance))
+		ctx.moveTo(...startLine.array())
+		ctx.lineTo(...startLine.add2(0, -(maximumHeight+0.15)*200).array())
+	}
+	for (let i = 0; i < maximumHeight+0.1; i += step) {
+		const startLine = cannonEnd.add2(0,-i*200)
+		ctx.moveTo(...startLine.array())
+		ctx.lineTo(...startLine.add2((finalDistance+0.15)*200).array())
+	}
+	ctx.stroke()
+
+	
+
 	ctx.save()
 	ctx.translate(170, 40)
 	ctx.rotate(angle)
 	//ctx.translate(-120, -40)
 	cannonCylinder.draw(ctx, -120, -40)
 	ctx.restore()
-	ctx.save()
-
 	cannonBody.draw(ctx, 10, 30)
-	ctx.restore()
-	ctx.translate(-10, -300)
-	const cannonEnd = new Vector(
-		10 + 170 + Math.cos(angle) * 150,
-		340 + Math.sin(angle) * 150
-	)
-	drawShape.circle(ctx, cannonEnd, 5)
 	
-	drawGraph(cannonEnd.array())
+	drawShape.circle(ctx, cannonEnd, 5, 'red')
+
+	ctx.resetTransform()
+
+	//drawGraph(cannonEnd.array())
 	requestAnimationFrame(loop)
 }
 loop()
