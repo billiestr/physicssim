708684bd086d3db809fc4c33a833cdd49a20ff0f
billiestr <billieagcave@gmail.com>
2024-08-23 16:05:46 +0100

    began circuit sim (place nodes)

--- simulations/circuit/script.js
+++ simulations/circuit/script.js
@@ -6,22 +6,49 @@ import { Asset } from '../../utils/image.js'
 const canvas = document.getElementById('main-canvas');
 const ctx = canvas.getContext('2d')
 
+const nodes = []
+
+let cursorPosition = Vector.ZERO;
+addEventListener("mousemove", (event) => {
+	cursorPosition = getCursorPosition(canvas, event)
+})
+
+class Node {
+	constructor(position) {
+		this.position = position;
+		this._hovered = false;
+		this._active = false;
+	}
+	overlaps(newNodePosition) {
+		console.log(newNodePosition)
+		return (this.position.distance(newNodePosition) < 20)
+	}
+	checkHover(mousePos) {
+		this._hovered = (this.position.distance(mousePos) <= 10)
+		return this._hovered
+	}
+	checkActive(){
+		this._active = this._hovered;
+		return this._active;
+	}
+	draw() {
+		if (this._hovered) {
+			drawShape.circle(ctx, this.position, 12, 'rgba(255, 10, 10, 0.5)')
+		}
+		if (this._active) {
+			drawShape.circle(ctx, this.position, 11.5, 'rgb(170, 0, 0)')
+		}
+		nodeAsset.draw(ctx, this.position.x, this.position.y, 0, true)
+	}
+}
 
 function resetCanvasResolution() {
     const rect = canvas.getBoundingClientRect();
 	canvas.width = rect.width;
 	canvas.height = rect.height;
+	draw()
 }
 resetCanvasResolution()
-
-addEventListener("resize", resetCanvasResolution)
-
-
-// number of pixels on the canvas
-ctx.fillStyle = "green";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-const nodeAsset = new Asset('../../assets/node.webp', new Vector(20, 20))
 
 function getCursorPosition(canvas, event) {
     const rect = canvas.getBoundingClientRect()
@@ -30,9 +57,26 @@ function getCursorPosition(canvas, event) {
     return new Vector(x, y)
 }
 
+addEventListener("resize", resetCanvasResolution)
+
+
+const nodeAsset = new Asset('../../assets/node.webp', new Vector(20, 20))
+
+function draw() {
+	ctx.fillStyle = "green";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	nodes.forEach(node => node.draw())
+}
+
 canvas.addEventListener("click", (event)=>{
-	const {x, y} = getCursorPosition(canvas, event)
-	nodeAsset.draw(ctx, x, y, 0, true)
+	nodes.forEach((node) => {node.checkActive(); node.checkActive();})
+	const newNodePosition = getCursorPosition(canvas, event)
+	if (nodes.every((node) => {
+		return !(node.overlaps(newNodePosition))
+	})) {
+		nodes.push(new Node(newNodePosition))
+	}
 })
 
 class Menu {
@@ -65,7 +109,11 @@ const time = new Time()
 }*/
 
 function loop() {
-	//requestAnimationFrame(loop)
+	nodes.forEach((node) => {
+		node.checkHover(cursorPosition)
+	})
+	draw()
+	requestAnimationFrame(loop)
 	//setTimeout(() => { loop(); }, 1000)
 }
 loop()
