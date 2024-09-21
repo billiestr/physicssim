e0a8ca88e0fd267b3b0fb42ba9847b24e664e146
billiestr <billieagcave@gmail.com>
2024-09-21 18:04:24 +0100

    circuit line

--- simulations/circuit/script.js
+++ simulations/circuit/script.js
@@ -7,6 +7,7 @@ const ctx = canvas.getContext('2d')
 
 const componentSize = new Vector(65, 34)
 const componentAssets = {
+	'battery': new Asset("../../assets/circuit/battery.png", componentSize),
 	'bulb-on': new Asset("../../assets/circuit/lighton.png", componentSize),
 	'bulb': new Asset("../../assets/circuit/lightoff.png", componentSize),
 	'ammeter': new Asset("../../assets/circuit/ammeter.png", componentSize),
@@ -22,16 +23,24 @@ canvas.addEventListener("click", (event) => {
 		cursor.heldComponent = null;
 		return;
 	}
+	components.forEach((component) => {
+		if (component.clickCheck(cursor.position)) {
+			cursor.activeNode = component
+		}
+	})
 	nodes.forEach((node) => { node.checkActive(); })
 	const newNodePosition = cursor.position;
 	if (nodes.every((node) => {
 		return !(node.overlaps(newNodePosition))
+	}) && components.every((component) => {
+		return !(component.overlaps(newNodePosition))
 	})) {
 		const newNode = new Node(newNodePosition)
 		newNode._hovered = true;
 		nodes.push(newNode)
 	}
 })
+
 
 const nodes = []
 const components = []
@@ -55,50 +64,32 @@ const cursor = {
 	}
 }
 
-class Component {
-	constructor(type, position, resistance) {
-		this.type = type
-		this.position = position;
-		this.resisitance = resistance;
-		this.nodeOne = new Node(this.position.add(new Vector(170)))
-		
-	}
-	draw(ctx) {
-		componentAssets[this.type].draw(ctx, ...this.position.array())
+const nodeOrder = {
+	list: [],
+	draw: function (ctx) {
+		let currentPoint = this.list[0];
+		for (let i = 1; i < this.list.length-1; i++) {
+			const nextPoint = this.list[i+1];
+			console.log(currentPoint)
+			if (currentPoint instanceof Component ||
+				nextPoint instanceof Component) {
+					i = i + 1
+					currentPoint = this.list[i+1]; 
+					continue;
+				}
+				
+			drawShape.line(ctx, currentPoint.position, nextPoint.position, "black");
+			currentPoint = nextPoint;
+		}
 	}
 }
 
-
-
-let mode = "default"
-
-const keyPressed = {
-	shift: false
-}
-
-addEventListener("keyup", ({ key }) => {
-	switch (key) {
-		case "w":
-			mode = mode != "drawWire" ? "drawWire" : "default"
-			break;
-		case "Escape":
-			mode = "default"
-			break;
-	}
-})
-
-
-
-addEventListener("mousemove", (event) => {
-	cursor.updatePosition(canvas, event)
-	nodes.forEach((node) => {
-		node.checkHover(cursor.position)
-	})
-})
-
+const nodeAsset = new Asset('../../assets/circuit/node.webp', new Vector(20, 20))
 class Node {
 	constructor(position) {
+		nodeOrder.list.push(this)
 		this.position = position;
+		this.next = null;
 		this._hovered = false;
 		this._active = false;
 	}
@@ -123,20 +114,69 @@ class Node {
 		nodeAsset.draw(ctx, this.position.x, this.position.y, 0, true)
 	}
 }
+class Component {
+	constructor(type, position, resistance) {
+		this.position = position;
+		this.nodeOne = new Node(this.position.add(new Vector(-32.5, 0)))
+		nodeOrder.list.push(this)
+		this.nodeTwo = new Node(this.position.add(new Vector(32.5, 0)))
+
+		this.type = type
+		this.resisitance = resistance;
+		
+		nodes.push(this.nodeOne, this.nodeTwo)
+		
+	}
+	clickCheck(clickPosition) {
+		return ((Math.abs(this.position.x - clickPosition.x) < 32.5) &&
+			(Math.abs(this.position.y - clickPosition.y) < 17));
+	}
+	overlaps(position) {
+		return (this.position.distance(position) < 65)
+	}
+	draw(ctx) {
+		componentAssets[this.type].draw(ctx, ...this.position.array(), 0, true)
+	}
+}
+components.push(new Component("battery", new Vector(405, 250), 0))
+
+let mode = "default"
+
+const keyPressed = {
+	shift: false
+}
+
+addEventListener("keyup", ({ key }) => {
+	switch (key) {
+		case "w":
+			mode = mode != "drawWire" ? "drawWire" : "default"
+			break;
+		case "Escape":
+			mode = "default"
+			cursor.heldComponent = null;
+			break;
+	}
+})
+
+
+
+addEventListener("mousemove", (event) => {
+	cursor.updatePosition(canvas, event)
+	nodes.forEach((node) => {
+		node.checkHover(cursor.position)
+	})
+})
+
+
 
 function resetCanvasResolution() {
 	const rect = canvas.getBoundingClientRect();
 	canvas.width = rect.width;
 	canvas.height = rect.height;
-	draw()
 }
 resetCanvasResolution()
 
 addEventListener("resize", resetCanvasResolution)
-
-
-const nodeAsset = new Asset('../../assets/circuit/node.webp', new Vector(20, 20))
-
 
 canvas.addEventListener("mousedown", (event) => {})
 canvas.addEventListener("mouseup", (event) => {})
@@ -163,13 +203,16 @@ const time = new Time()
 
 
 function draw() {
+	if (cursor.activeNode) {
+		drawShape.circle(ctx, cursor.activeNode.position, 20, 'rgba(200, 0, 0, 0.1)')
+	}
 	nodes.forEach(node => node.draw())
 	components.forEach(component => component.draw(ctx))
 
 	if (cursor.heldComponent) {
-		componentAssets[cursor.heldComponent].draw(ctx, ...cursor.position.array())
+		componentAssets[cursor.heldComponent].draw(ctx, ...cursor.position.add2(-32.5, -16).array())
 	}
-	
+	nodeOrder.draw(ctx)
 }
 
 function loop() {
@@ -180,7 +223,6 @@ function loop() {
 	
 	draw()
 	
-	//requestAnimationFrame(loop)
-	setTimeout(() => { loop(); }, 500)
+	requestAnimationFrame(loop)
 }
 loop()
